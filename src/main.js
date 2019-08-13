d85aa5fa136b8282598b3f0efccbc02ab9805136
eremina-ekaterina <eremina@uchi.ru>
2019-08-13 09:19:12 +0300

    prove js and html

--- src/main.js
+++ src/main.js
@@ -11,20 +11,20 @@ const renderComponent = (parent, child, place) => {
 };
 
 const mainContainer = document.querySelector(`.page-body`);
-const infoContainer = mainContainer.querySelector(`.trip-info__main`);
+const infoContainer = mainContainer.querySelector(`.trip-main__trip-info`);
 const menuContainer = mainContainer.querySelector(`.trip-main__trip-controls`);
-const filterContainer = mainContainer.querySelector(`.trip-events__trip-sort`);
-const cardEditContainer = mainContainer.querySelector(`.trip-events__item`);
-const cardTasksContainer = mainContainer.querySelector(`.trip-days`);
+// const filterContainer = mainContainer.querySelector(`.trip-events__trip-sort`);
+const cardEditContainer = mainContainer.querySelector(`.page-body__container .trip-events`);
+// const cardTasksContainer = mainContainer.querySelector(`.trip-days`);
 
 const renderMockComponents = () => {
-  renderComponent(infoContainer, makeInfoTemplate(), `beforeend`);
+  renderComponent(infoContainer, makeInfoTemplate(), `afterbegin`);
   renderComponent(menuContainer, makeMenuTemplate(),`beforeend`);
-  renderComponent(filterContainer, makeFilterTemplate(),`beforeend`);
-  renderComponent(cardEditContainer, makeCardEditTemplate(),`beforeend`);
+  renderComponent(menuContainer, makeFilterTemplate(),`beforeend`);
+  renderComponent(cardEditContainer, makeCardEditTemplate(),`afterbegin`);
 
   for (let i = 1; i <= CARDS_COUNT; i++) {
-    renderComponent(cardTasksContainer, makeCardTemplate(),`beforeend`);
+    renderComponent(cardEditContainer, makeCardTemplate(),`beforeend`);
   }
 
 };
