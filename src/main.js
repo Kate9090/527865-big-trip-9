57683be92e0186f53d3bd2d5f137e393dc2125ab
eremina-ekaterina <eremina@uchi.ru>
2019-08-15 12:20:07 +0300

    proves notices from tutor

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import {makeCardEditTemplate} from '../src/components/card-edit'; //+
+import {makeCardEditTemplate} from '../src/components/card-edit';
 import {makeCardTemplate} from '../src/components/card';
-import {makeFilterTemplate} from '../src/components/filter'; //+
-import {makeMenuTemplate} from '../src/components/menu'; //+
+import {makeFilterTemplate} from '../src/components/filter';
+import {makeMenuTemplate} from '../src/components/menu';
 import {makeInfoTemplate} from '../src/components/info';
 
 const CARDS_COUNT = 3;
@@ -9,24 +9,19 @@ const CARDS_COUNT = 3;
 const renderComponent = (parent, child, place) => {
   parent.insertAdjacentHTML(place, child);
 };
-
 const mainContainer = document.querySelector(`.page-body`);
 const infoContainer = mainContainer.querySelector(`.trip-main__trip-info`);
 const menuContainer = mainContainer.querySelector(`.trip-main__trip-controls`);
-// const filterContainer = mainContainer.querySelector(`.trip-events__trip-sort`);
 const cardEditContainer = mainContainer.querySelector(`.page-body__container .trip-events`);
-// const cardTasksContainer = mainContainer.querySelector(`.trip-days`);
 
 const renderMockComponents = () => {
   renderComponent(infoContainer, makeInfoTemplate(), `afterbegin`);
   renderComponent(menuContainer, makeMenuTemplate(),`beforeend`);
   renderComponent(menuContainer, makeFilterTemplate(),`beforeend`);
   renderComponent(cardEditContainer, makeCardEditTemplate(),`afterbegin`);
-
   for (let i = 1; i <= CARDS_COUNT; i++) {
     renderComponent(cardEditContainer, makeCardTemplate(),`beforeend`);
   }
-
 };
 
 renderMockComponents();
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    publicPath: 'http://localhost:8080/',
+    publicPath: 'http://localhost:8090/',
     compress: true,
     watchContentBase: true
-    }
+  }
 };
